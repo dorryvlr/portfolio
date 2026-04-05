5f8081f795abb6fc10f170819cab2bd4e944266f
dorryvlr <imdorryvlr2601@gmail.com>
2026-04-05 18:25:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/ui/Navbar.jsx";
+import Hero from "./components/figma/Hero.jsx";
 
 function App() {
   return (
     <div>
       <h1>WORKING 🔥</h1>
       <h1>My Portfolio 🚀</h1>
+
       <Navbar />
+      <Hero />
     </div>
   );
 }
