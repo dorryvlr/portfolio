e0c1d28f1d8cddfe29e28277c0f89c58d809ea52
dorryvlr <imdorryvlr2601@gmail.com>
2026-04-05 18:27:31 +0530

    Create VideoSection.jsx

---
+++ src/components/figma/VideoSection.jsx
@@ -0,0 +1,13 @@
+function VideoSection() {
+  return (
+    <section>
+      <h2>My Work 🎬</h2>
+
+      <video width="300" controls>
+        <source src="/video.mp4" type="video/mp4" />
+      </video>
+    </section>
+  );
+}
+
+export default VideoSection;
