53c883b314b0137c38276c8dccee9346ff9fc5a0
dorryvlr <imdorryvlr2601@gmail.com>
2026-04-05 18:23:57 +0530

    Create Hero.jsx

---
+++ src/components/figma/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h1>I am a Video Editor 🎬</h1>
+      <p>I create reels, ads and cinematic edits</p>
+    </section>
+  );
+}
+
+export default Hero;
